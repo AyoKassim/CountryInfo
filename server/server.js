4148bf7a5c0d5e0582707dfdd8788691c12f8a8c
Ayo <68858519+AyoKassim@users.noreply.github.com>
2023-11-29 15:13:47 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const port = 9000;
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(cors())
