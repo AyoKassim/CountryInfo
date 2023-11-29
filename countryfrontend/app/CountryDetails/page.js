768e9ad3cd098b7feb4981b9aec7ec14d7279f85
Ayo <68858519+AyoKassim@users.noreply.github.com>
2023-11-29 19:50:42 +0000

    Update page.js

--- countryfrontend/app/CountryDetails/page.js
+++ countryfrontend/app/CountryDetails/page.js
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const LoadData = async () => {
-    const countryResponse = await axios.get("http://country-info-1re6xd25a-kas-projects-8e5ea42c.vercel.app/countryinfo");
+    const countryResponse = await axios.get("https://country-info-1re6xd25a-kas-projects-8e5ea42c.vercel.app/countryinfo");
     console.log(countryResponse);
     return countryResponse;
 }
